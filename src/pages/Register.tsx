e5d6f904b8d9ba40a2799f5f2e1b40889704f16d
tamer22689 <130790311+tamer22689@users.noreply.github.com>
2023-05-24 20:05:03 +0300

    update

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,17 @@
 import { Button, Input } from "@chakra-ui/react"
 import "./Register.css"
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Register = () => {
+
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmpassword, setConfirmpassword] = useState('');
+    
     const navigate = useNavigate(); 
     const routeChange = () =>{ 
         const path = "/"; 
@@ -12,12 +21,18 @@ const Register = () => {
     <>
     <h1>Register</h1>
             <br></br>
-            <Input placeholder="first name"/>
-            <Input placeholder="last name"/>
-            <Input placeholder="user name"/>
-            <Input placeholder="email"/>
-            <Input placeholder="password"/>
-            <Input placeholder="confirm password"/>
+            <Input onChange={event => setFirstname(event.target.value)} placeholder="first name"/>
+            <div className="first name">{firstname}</div>
+            <Input onChange={event => setLastname(event.target.value)} placeholder="last name"/>
+            <div className="last name">{lastname}</div>
+            <Input onChange={event => setUsername(event.target.value)} placeholder="user name"/>
+            <div className="user name">{username}</div>
+            <Input onChange={event => setEmail(event.target.value)} placeholder="email"/>
+            <div className="email">{email}</div>
+            <Input onChange={event => setPassword(event.target.value)} placeholder="password"/>
+            <div className="password">{password}</div>
+            <Input onChange={event => setConfirmpassword(event.target.value)} placeholder="confirm password"/>
+            <div className="confirm password">{confirmpassword}</div>
             <Button onClick={routeChange}>submit</Button>
     </>
   );
